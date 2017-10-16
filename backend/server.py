25524d56b9b035bf1fc0de66ac23e5584b00b618
D.Ma <dana.ma537@gmail.com>
2017-10-17 08:14:46 +1100

    Refactor duplicate error handling into decorator

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, make_response
+from functools import wraps
 import json
 from requests.exceptions import ConnectTimeout
 
@@ -17,12 +18,20 @@ def _create_response(payload, status_code):
     response.headers["Access-Control-Allow-Origin"] = "*"
     return response
 
-def _lookup_ticket(ticket_id):
-    ticket_json = app.client.get_ticket(ticket_id)
-    ticket = _extract_fields(ticket_json)
-    return _create_response(json.dumps(ticket), 200)
+def error_handler(f):
+    @wraps(f)
+    def wrapper(*args, **kw_args):
+        try:
+            return f(*args, **kw_args)
+        except ConnectTimeout:
+            return _create_response("Zendesk API appears to be unavailable...", 503)
+        except Exception as e:
+            app.logger.error(str(e))
+            return _create_response("Internal server error", 500)
+    return wrapper
 
 @app.route("/ticket/<ticket_id>")
+@error_handler
 def lookup_ticket(ticket_id):
     """ Endpoint to look up ticket info 
     Args:
@@ -30,29 +39,17 @@ def lookup_ticket(ticket_id):
     
     Returns:
         Ticket info """
-    try:
-        return _lookup_ticket(ticket_id)
-    except ConnectTimeout:
-        return _create_response("Zendesk API appears to be unavailable...", 503)
-    except Exception as e:
-        app.logger.error(str(e))
-        return _create_response("Internal server error", 500)
+    ticket_json = app.client.get_ticket(ticket_id)
+    ticket = _extract_fields(ticket_json)
+    return _create_response(json.dumps(ticket), 200)
 
-def _list_tickets():
+@app.route("/tickets")
+@error_handler
+def list_tickets():
+    """ Endpoint to list all tickets """
     tickets_json = app.client.list_tickets()
     tickets = [_extract_fields(t) for t in tickets_json]
     return _create_response(json.dumps(tickets), 200)
-
-@app.route("/tickets")
-def list_tickets():
-    """ Endpoint to list all tickets """
-    try:
-        return _list_tickets()
-    except ConnectTimeout:
-        return _create_response("Zendesk API appears to be unavailable...", 503)
-    except Exception as e:
-        app.logger.error(str(e))
-        return _create_response("Internal server error", 500)
 
 if __name__ == "__main__":
     app.run()
